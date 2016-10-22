a58a2bd6b4d07e1b6272f207e1b84f4cb4f19141
Projjol Banerji <projjol.banerji@idfy.com>
2016-10-22 18:29:58 +0530

    calendar for flock

---
+++ calendar_working.rb
@@ -0,0 +1,51 @@
+
+OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'
+APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'
+CLIENT_SECRETS_PATH = 'client_secret.json'
+CREDENTIALS_PATH = File.join(Dir.home, '.credentials',
+                             "calendar-ruby-quickstart.yaml")
+SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+
+def authorize
+  FileUtils.mkdir_p(File.dirname(CREDENTIALS_PATH))
+
+  #this is the oauth code. I think it's similar to the method you wrote
+  client_id = Google::Auth::ClientId.from_file(CLIENT_SECRETS_PATH)
+  token_store = Google::Auth::Stores::FileTokenStore.new(file: CREDENTIALS_PATH)
+  authorizer = Google::Auth::UserAuthorizer.new(
+    client_id, SCOPE, token_store)
+  user_id = 'default'
+  credentials = authorizer.get_credentials(user_id)
+  if credentials.nil?
+    url = authorizer.get_authorization_url(
+      base_url: OOB_URI)
+    puts "Open the following URL in the browser and enter the " +
+         "resulting code after authorization"
+    puts url
+    code = gets
+    credentials = authorizer.get_and_store_credentials_from_code(
+      user_id: user_id, code: code, base_url: OOB_URI)
+  end
+  credentials
+end
+
+# calls method above ^
+service = Google::Apis::CalendarV3::CalendarService.new
+service.client_options.application_name = APPLICATION_NAME
+service.authorization = authorize
+
+# this will fetch events
+# returns object with data for calender event. When I tried with my gmail account it fetched the only one event it is supposed to have.
+calendar_id = 'primary'
+response = service.list_events(calendar_id,
+                               max_results: 10,
+                               single_events: true,
+                               order_by: 'startTime',
+                               time_min: Time.now.iso8601)
+
+puts "Upcoming events:"
+puts "No upcoming events found" if response.items.empty?
+response.items.each do |event|
+  start = event.start.date || event.start.date_time
+  puts "- #{event.summary} (#{start})"
+end
